13c9f0bf90cf669f0330477ad78c55dcd0791b3b
Darshan Choudhary <darshanchoudhary2007@gmail.com>
2024-07-29 23:09:15 +0530

    Updated game.js

--- game.js
+++ game.js
@@ -35,6 +35,7 @@ console.log("start");
 const player = "p"
 const trash = "t"
 const dustbin = "b"
+const outline = "o"
 
 setLegend(
   [ player, bitmap`
@@ -87,7 +88,24 @@ setLegend(
 ...000.00.00....
 ...0.000.000....
 ...000000000....
-................` ]
+................` ],
+  [ outline, bitmap`
+0000000000000000
+0000000000000000
+00CCCCCCCCCCCC00
+00CCCCCCCCCCCC00
+00CC00000000CC00
+00CC00000000CC00
+00CC00CCCC00CC00
+00CC00CCCC00CC00
+00CC00CCCC00CC00
+00CC00CCCC00CC00
+00CC00000000CC00
+00CC00000000CC00
+00CCCCCCCCCCCC00
+00CCCCCCCCCCCC00
+0000000000000000
+0000000000000000`],
 
 )
 
@@ -96,6 +114,10 @@ setSolids([player,trash])
 let level = 0
 const levels = [
   map`
+oooooooooooooo
+o............o
+o............o
+oooooooooooooo
 ..............
 ..............
 .....p..t.....
@@ -103,12 +125,16 @@ const levels = [
 .....t........
 .........b....`,
   map`
-......
-.tt.t.
-.p.t.t
-.t....
-..t.t.
-.t..b.`,
+oooooooooooooo
+o............o
+o............o
+oooooooooooooo
+..............
+.....tt.t.....
+.....p.t.t....
+.....t........
+......t.t.....
+.....t..b.....`,
   
   
 ]
@@ -143,8 +169,9 @@ onInput("j",()=>{
 afterInput(() => {
   number_of_trash = getAll(trash).length
   addText("No. of trash \nremaining = "+number_of_trash,{
-    x:0,
-    y:2
+    x : 4,
+    y : 3,
+    size : 0.5
   })
   
   const dustbin_x = getFirst(dustbin).x;
@@ -164,3 +191,149 @@ afterInput(() => {
     setMap(levels[level])
   }
 })
+
+let currentLevelText;
+
+let mainMenuTitle = `
+  Up  Down
+  
+  Top-Down
+`;
+
+let mainMenuOptions = `
+  Start Game
+  ----------
+  
+  Guide
+  -----
+`;
+
+let backButton = `
+Back
+----
+`;
+
+let tutorial1 = `
+Double-click
+the jump button 
+to get higher
+`;
+
+let tutorial2 = `
+Get on 
+top the
+Gravity 
+Block to
+activate`;
+
+let errorSpawn = `
+Error: Couldn't 
+find empty tile
+for player spawn`;
+
+let deathText = `You died`;
+
+let finishText = `
+You completed
+  the game!
+
+
+  
+ Thanks for
+  playing!`;
+
+// Guide Texts
+let menuGuide = `Press   
+
+to activate`;
+// Controls
+let upLGuide = `Moves player 
+upward in 
+Top-down mode`;
+let leftLGuide = `Moves player to 
+the left`;
+let downLGuide = `Moves player 
+downward in
+Top-down mode`;
+let rightLGuide = `Moves player to 
+the right`;
+let upRGuide = `Makes player jump`;
+let leftRGuide = `Returns to menu
+(Level is saved)`;
+let downRGuide = `Makes player jump,
+also acts as the
+back button in
+the menu`;
+let rightRGuide = `Confirm menu
+selection, also 
+acts as level skip`;
+// Blocks
+let playerGuide = `It's you!`;
+let blockGuide = `A solid platform,
+good for standing
+on`;
+let magicBlockGuide = `???`;
+let flagGuide = `Get near it
+and you complete
+the level`;
+let gravityBlockGuide = `Stand on this
+block and watch
+as you flip
+upside down`;
+
+function gameFinished(winner) {
+  gameover = true
+  if (winner == "x") {
+    addText(`Game over. X won!`, {
+      y: 5,
+      size: 1,
+      color: color`3`
+    })
+  } else if (winner == "o") {
+    addText(`Game over. O won!`, {
+      y: 5,
+      color: color`4`
+    })
+  } else {
+    addText(`Game over.`, {
+      x: 6,
+      y: 3,
+      color: color`9`
+    })
+    addText(`It's a draw!`, {
+      x: 5,
+      y: 5,
+      color: color`9`
+    })
+  }
+  addText(`Press J to restart`, {
+    x: 1,
+    y: 10,
+    size: 1,
+    color: color`H`
+  })
+}
+
+function resetCurrentLevel() {
+  if (!isInMainMenu && isGameStarted) {
+    if (easyLevels.includes(currentLevel)) {
+      setMap(currentLevel);
+    } else if (mediumLevels.includes(currentLevel)) {
+      setMap(currentLevel);
+    } else if (hardLevels.includes(currentLevel)) {
+      setMap(currentLevel);
+    }
+  }
+}
+
+// Start game functions for each difficulty
+function startEasyGame() {
+  currentLevel = easyLevels[easyLevel];
+  setMap(currentLevel);
+  clearText();
+  isGameStarted = true;
+
+  afterInput(() => {
+    checkCompletion(easyLevels);
+  });
+}
